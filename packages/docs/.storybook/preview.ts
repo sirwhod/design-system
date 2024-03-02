81d58b741c887d9f76e0ca17d5d62a05801dbde2
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-03-02 16:32:23 -0300

    class: Documentação de cores

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming"
 
 const preview: Preview = {
   parameters: {
@@ -12,6 +13,9 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    docs: {
+      theme: themes.dark
+    }
   },
 };
 
